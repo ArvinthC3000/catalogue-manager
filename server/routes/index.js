5d0e60438e1717ab5a628e2238970cd260b64fc0
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-17 19:35:21 +0530

    Route Handler

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const router = express.Router();
 
+// Product inserting handler
+
+// @type    POST
+// @route   api/add
+// @desc    Adding new product 
+// @access  Public
+
 router.get('/', (req, res)=>{
     res.send('Test router')
 })
 
-router.use('/test', require('./test'))
+router.use('/add', require('./addProduct'))
 
 
 module.exports = router; 
