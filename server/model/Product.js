fd4018f6d5b67dfb65fb81ca91bcb4c452fcdd66
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-17 19:34:43 +0530

    Schema for Product

---
+++ server/model/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    product: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    parentCategory: {
+        type: String
+    },
+    specification: {
+        type: Object,
+        required: true
+    }
+})
+
+const Product =  mongoose.model('products', ProductSchema)
+
+module.exports = Product
