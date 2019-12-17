1b31a2d727c7c4c5d19e31b55e2f76c903dd4519
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-17 15:15:05 +0530

    Connencting db and route handlling middleware

--- server/index.js
+++ server/index.js
@@ -4,3 +4,31 @@ const bodyparser = require('body-parser')
 const app =  express()
 const cors = require('cors')
 const PORT = process.env.PORT || 4000
+
+// DB config
+
+const db = require('./configs/keys').url
+
+// Connect to mongoDB
+
+mongoose.connect(db, { useNewUrlParser:true, useUnifiedTopology: true })
+.then(()=> console.log('MongoDB connected...'))
+.catch(err=>console.log(err))
+
+// Bodyparser 
+
+app.use(express.urlencoded({ extended:false }))
+app.use(bodyparser.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("server is up and running")
+})
+
+
+app.use("/api", require('./routes'))
+
+
+
+
+app.listen(PORT, console.log('Server is listening at port '+PORT))
