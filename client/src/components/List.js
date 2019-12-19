62bf1a885e718c77d189a63418628d9da19cff82
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-19 18:53:07 +0530

    List container

---
+++ client/src/components/List.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Axios from 'axios'
+import ListComponent from './ListComponent'
+
+class List extends Component {
+    constructor(){
+        super()
+        this.state={
+            data : []
+        }
+    }
+    async componentDidMount(){
+        await Axios.get('http://localhost:4000/api/view').then(async res=>{
+            const data = []
+            for(let i = 0;i < res.data.data.length; i++){
+                data.push(res.data.data[i])
+            }
+            this.setState({data:data})
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <ListComponent  data={this.state.data} />
+            </div>
+        )
+    }
+}
+
+export default List
