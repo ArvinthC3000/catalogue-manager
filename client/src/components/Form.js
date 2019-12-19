f3a4905fbc4f0b2f42d3bfc84b62b1d0241df8e5
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-19 18:54:44 +0530

    setting state and handlers

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,62 +1,93 @@
 import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
+import Axios from 'axios'
 
 class Form extends Component {
+    constructor(){
+        super()
+        this.state = {
+            productName: '',
+            category:'',
+            brand:'',
+            parentCategory:'',
+            specification:''
+        }
+    }
+
+    handleChange = async (e) =>{
+        const { name, value } = e.target
+        await this.setState({ [name]: value })
+    }
+
+    handleSubmit = async (e) =>{
+        e.preventDefault()
+        await Axios.post('http://localhost:4000/api/add',this.state)
+        .then(this.setState({
+                productName: '',
+                category:'',
+                brand:'',
+                parentCategory:'',
+                specification:''
+            })
+        )
+    }
+    
     render() {
         return (
             <div className='formDiv'>
-                <form /*onSubmit={this.handleSubmit}*/ className='form'>
+                <form onSubmit={this.handleSubmit} className='form'>
                     
                     <label>Product Name:</label>
                     <input 
                         type="text" 
-                        // value={this.state.firstName} 
-                        name="firstName" 
+                        value={this.state.productName} 
+                        name="productName" 
                         placeholder="Product" 
-                        // onChange={this.handleChange} 
+                        onChange={this.handleChange} 
                         />
                     <br />
                     <label>Brand:</label>
                     <input 
                         type="text" 
-                        // value={this.state.lastName} 
-                        name="lastName" 
+                        value={this.state.brand} 
+                        name="brand" 
                         placeholder="Brand" 
-                        // onChange={this.handleChange} 
+                        onChange={this.handleChange} 
                         />
                     <br />
                     <label>Category:</label>
                     <input 
                         type="text" 
-                        // value={this.state.lastName} 
-                        name="lastName" 
+                        value={this.state.category} 
+                        name="category" 
                         placeholder="Category" 
-                        // onChange={this.handleChange} 
+                        onChange={this.handleChange} 
                         />
                     <br />
                     <label>Parent Category:</label>
                     <input 
                         type="text" 
-                        // value={this.state.lastName} 
-                        name="lastName" 
+                        value={this.state.parentCategory} 
+                        name="parentCategory" 
                         placeholder="Parent Category" 
-                        // onChange={this.handleChange} 
+                        onChange={this.handleChange} 
                         />
                     <br />
                     <label>Specification:</label>
                     <input 
                         type="text" 
-                        // value={this.state.lastName} 
-                        name="lastName" 
+                        value={this.state.specification} 
+                        name="specification" 
                         placeholder="Specification" 
-                        // onChange={this.handleChange} 
+                        onChange={this.handleChange} 
                     />
                     <br />
                     
                     <p>Please confirm the details before submitting</p>
-                    <Button variant="secondary">Submit</Button>
+                    <button>
+                        <Button variant="secondary">Submit</Button>
+                    </button>
                 </form>
-                <Button variant="dark">Go Back</Button>
             </div>
         )
     }
